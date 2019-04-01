acdc8ed7f1a2fb65d96319a44ade5ccc0ad2173d
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-04-01 10:58:02 -0400

    API route to populate DB from send button in modal

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -29,7 +29,17 @@ module.exports = function(app) {
   // Get user info by chosen Id and populates the chosen template
   app.get("/template1/:id", function(req, res) {
     db.users.findOne({ where: { id: req.params.id } }).then(function(userInfo) {
-      console.log("=============================",userInfo);
+      // console.log("=============================",userInfo);
+      var coordArr=userInfo.dataValues.coordinates.split(",");
+      var schedArr=userInfo.dataValues.schedule.split(",");
+      var pictArr=userInfo.dataValues.pictures.split(",");
+      userInfo.dataValues.latitude = coordArr[0];
+      userInfo.dataValues.longitude = coordArr[1];
+      userInfo.dataValues.schedule = schedArr;
+      userInfo.dataValues.pictures1 = pictArr[0];
+      userInfo.dataValues.pictures2 = pictArr[1];
+      userInfo.dataValues.pictures3 = pictArr[2];
+      console.log("What's this? =============================",userInfo.dataValues);
       res.render("template1", {userInfo: userInfo});
     });
   });
@@ -38,16 +48,28 @@ module.exports = function(app) {
       var coordArr=userInfo.dataValues.coordinates.split(",");
       var schedArr=userInfo.dataValues.schedule.split(",");
       var pictArr=userInfo.dataValues.pictures.split(",");
-      userInfo.dataValues.coordinates = coordArr;
+      userInfo.dataValues.latitude = coordArr[0];
+      userInfo.dataValues.longitude = coordArr[1];
       userInfo.dataValues.schedule = schedArr;
-      userInfo.dataValues.pictures = pictArr;
+      userInfo.dataValues.pictures1 = pictArr[0];
+      userInfo.dataValues.pictures2 = pictArr[1];
+      userInfo.dataValues.pictures3 = pictArr[2];
       // console.log("What's this? =============================",userInfo.dataValues);
       res.render("template2", {userInfo: userInfo});
     });
   });
   app.get("/template3/:id", function(req, res) {
     db.users.findOne({ where: { id: req.params.id } }).then(function(userInfo) {
-      console.log(userInfo);
+      var coordArr=userInfo.dataValues.coordinates.split(",");
+      var schedArr=userInfo.dataValues.schedule.split(",");
+      var pictArr=userInfo.dataValues.pictures.split(",");
+      userInfo.dataValues.latitude = coordArr[0];
+      userInfo.dataValues.longitude = coordArr[1];
+      userInfo.dataValues.schedule = schedArr;
+      userInfo.dataValues.pictures1 = pictArr[0];
+      userInfo.dataValues.pictures2 = pictArr[1];
+      userInfo.dataValues.pictures3 = pictArr[2];
+      // console.log(userInfo);
       res.render("template3", {userInfo: userInfo});
     });
   });
